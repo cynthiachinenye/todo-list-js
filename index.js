473451f9ba2af96d93b048fd036e43ff6471b75b
cynthiachinenye <cynthiachiagu@gmail.com>
2023-11-29 11:43:03 +0100

    new

--- index.js
+++ index.js
@@ -2,10 +2,12 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton= document.querySelector('.todo-btn');
 const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector('.filter-todo')
 
 // event listeners
 todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', deleteCheck);
+filterOption.addEventListener('click', filterTodo)
 
 // function
 function addTodo(event){
@@ -64,4 +66,31 @@ if(item.classList[0] === "complete-btn"){
     const todo = item.parentElement;
     todo.classList.toggle("completed");
 }
+ }
+
+ function filterTodo(e) {
+    const todos = todoList.childNodes;
+    todos.forEach(function(todo){
+        switch (e.target.value) {
+            case 'all':
+                todo.style.display="flex";
+            break;
+            case 'completed':
+                if(todo.classList.contains('completed')){
+                    todo.style.display="flex";
+                }else{
+                    todo.style.display='none';
+                }
+                break;
+                case 'uncompleted':
+                    if(!todo.classList.contains('completed')){
+                      todo.style.display="flex";
+
+                    }else{
+                        todo.syle.display="none"
+                    }
+                    break;
+        }
+    })
+
  }
