ec87099edd6d4d54331796b19e5215fd876dee64
cynthiachinenye <cynthiachiagu@gmail.com>
2023-11-29 09:52:29 +0100

    new

---
+++ index.js
@@ -0,0 +1,39 @@
+// selector
+const todoInput = document.querySelector('.todo-input');
+const todoButton= document.querySelector('.todo-btn');
+const todoList = document.querySelector('.todo-list');
+
+// event listeners
+todoButton.addEventListener('click', addTodo);
+
+// function
+function addTodo(event){
+
+    event.preventDefault();
+    // todo div
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add("todo")
+    // create li
+    const newTodo = document.createElement('li');
+    newTodo.innerText='hello';
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+
+    // check mark button
+    const completeButton =document.createElement('button');
+    completeButton.innerHTML='<i class="fas fa-check"></i>';
+    completeButton.classList.add("complete-btn");
+    todoDiv.appendChild(completeButton);
+
+    // check trash button
+
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+
+    // append to list
+    todoList.appendChild(todoDiv)
+
+
+}
